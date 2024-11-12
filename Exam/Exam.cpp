306dde7be2a5084092d2ce4f43058b62ab2052e3
4b3g0015@office.stust.edu.tw <4b3g0015@office.stust.edu.tw>
2024-11-12 22:09:45 +0800

    ex2

--- Exam/Exam.cpp
+++ Exam/Exam.cpp
@@ -1,20 +1,40 @@
-﻿// Exam.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
-
-#include <iostream>
+﻿#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n = 1;
+	int sum = 0;
+	cout << "請輸入一個正整數n: ";
+	cin >> n;
+
+	// for loop
+	for (int i = 1; i <= n; i++) {
+		if (i % 2 == 0) sum += i;
+	}
+	
+	sum = 0;
+	for (int i = 2; i <= n; i += 2) {
+		sum += i;
+	}
+	cout << "For loop: 1~" << n << "之間所有偶數的和為: " << sum << endl;
+	/*
+	// while loop
+	int i = 1;
+	sum = 0;
+	while (i <= n) {
+		if (i % 2 == 0) sum += i;
+		i++;
+	}
+	cout << "While loop: 1~" << n << "之間所有偶數的和為: " << sum << endl;
+
+	// do-while loop
+	int i = 2;
+	sum = 0;
+	do {
+		sum += i;
+		i += 2;
+	} while (i <= n);
+	cout << "Do-While loop: 1~" << n << "之間所有偶數的和為: " << sum << endl;
+	*/
 }
-
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
-
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
