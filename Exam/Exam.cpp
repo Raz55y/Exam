ddf6505af4b2bf092fc18dd37982ba012bffc7e5
4b3g0015@office.stust.edu.tw <4b3g0015@office.stust.edu.tw>
2024-11-12 22:20:34 +0800

    ex3

--- Exam/Exam.cpp
+++ Exam/Exam.cpp
@@ -1,40 +1,29 @@
 ﻿#include <iostream>
 using namespace std;
 
+int factorial(int n) {
+	if (n == 0) return 1;
+	else return n * factorial(n - 1);
+}
+
 int main()
 {
-	int n = 1;
-	int sum = 0;
-	cout << "請輸入一個正整數n: ";
-	cin >> n;
+	int num;
 
-	// for loop
-	for (int i = 1; i <= n; i++) {
-		if (i % 2 == 0) sum += i;
-	}
-	
-	sum = 0;
-	for (int i = 2; i <= n; i += 2) {
-		sum += i;
-	}
-	cout << "For loop: 1~" << n << "之間所有偶數的和為: " << sum << endl;
-	/*
-	// while loop
-	int i = 1;
-	sum = 0;
-	while (i <= n) {
-		if (i % 2 == 0) sum += i;
-		i++;
-	}
-	cout << "While loop: 1~" << n << "之間所有偶數的和為: " << sum << endl;
+	cout << "輸入一個正整數: ";
+	cin >> num;
 
-	// do-while loop
-	int i = 2;
-	sum = 0;
-	do {
-		sum += i;
-		i += 2;
-	} while (i <= n);
-	cout << "Do-While loop: 1~" << n << "之間所有偶數的和為: " << sum << endl;
-	*/
+	// 非遞迴計算n!
+/*	int result = 1;
+	for (int i = 1; i <= num; i++)
+	{
+		result *= i;
+		//result = result * i;
+	}
+	cout << "非遞迴版本：" << num << "! = " << result << endl;
+*/
+	// 遞迴計算n!
+	int result = factorial(num);
+	cout << "遞迴版本：" << num << "! = " << result << endl;
+
 }
